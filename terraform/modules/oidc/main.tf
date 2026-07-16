6d83b4927b915c5bda016725a2cc936fec112793
Rafael Cardoso <rafacardoso@users.noreply.github.com>
2026-07-16 10:10:52 -0300

    feat: add OIDC module for GitHub Actions authentication

---
+++ terraform/modules/oidc/main.tf
@@ -0,0 +1,34 @@
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = data.tls_certificate.github.url
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "GitHubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/*"
+          }
+        }
+      }
+    ]
+  })
+}
---
+++ terraform/modules/oidc/variables.tf
@@ -0,0 +1,5 @@
+variable "github_org" {
+  description = "GitHub organization or username"
+  type        = string
+  default     = "raafa001"
+}
